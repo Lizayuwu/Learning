397e720a999b379cfe5f517ecf4ce042d8a8fd04
Lucas Didek <31461517+Rakanamatata@users.noreply.github.com>
2019-06-26 11:27:08 -0300

    added a simple clock in html/js

---
+++ exercises/misc/cheap-clock/js/main.js
@@ -0,0 +1,35 @@
+! function create(){ 
+	main = document.querySelector("main")
+	time = document.querySelector(".time")
+	inter = setInterval( (e) => {
+		date = new Date()
+		hour = date.getHours()
+		minutes = date.getMinutes()
+		seconds = date.getSeconds()
+		time.innerText = `${hour.toString().length == 1 ? "0" + hour.toString() : hour}:${minutes.toString().length == 1 ? "0" + minutes.toString() : minutes }:${seconds.toString().length == 1 ? "0" + seconds.toString() : seconds}`
+		main.style.backgroundColor = getRandomColor('hex')
+	}, 1000)
+} ()
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//@returns string : colour
+//hex format = #ffffff
+function getRandomColor(type) {
+	if(type === "hex") {
+		hex1 = getRandomIntInclusive(0, 255).toString(16)
+		hex2 = getRandomIntInclusive(0, 255).toString(16)
+		hex3 = getRandomIntInclusive(0, 255).toString(16)
+		hex1 = hex1.length == 1 ? "0"+hex1 : hex1
+		hex2 = hex2.length == 1 ? "0"+hex2 : hex2
+		hex3 = hex3.length == 1 ? "0"+hex3 : hex3
+		return `#${hex1 + hex2 + hex3}`
+	} else {
+		return getRandomIntInclusive(0, 255)
+	}
+
+}
