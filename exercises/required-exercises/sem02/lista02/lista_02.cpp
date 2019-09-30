2506b00f1d0ac477e127a416948125dfdfa21077
Rakanamatata <lucasdidek447@gmail.com>
2019-09-30 08:05:41 -0300

    Started required exercise list

---
+++ exercises/required-exercises/sem02/lista02/lista_02.cpp
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    return 0;
+}
+
+void ex01() {
+    unsigned int addresses;
+    unsigned int addressBitSize;
+
+    printf("How many adresses does your computer have");
+    printf("Starting exercise exercise 01:\n");
+
+    return;    
+}
+
+void ex02() {
+    printf("Starting exercise exercise 02:\n");
+
+    return;    
+}
+
+void ex03() {
+    printf("Starting exercise exercise 03:\n");
+
+    return;    
+}
+
+void ex04() {
+    printf("Starting exercise exercise 04:\n");
+
+    return;    
+}
+
+char* convertToNotation(int number) {
+
+}
