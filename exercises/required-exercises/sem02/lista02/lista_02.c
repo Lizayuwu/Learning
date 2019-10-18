59bdb63e8613230bca44115734fee9ffe6e2ee64
Rakanamatata <lucasdidek447@gmail.com>
2019-10-18 20:32:30 -0300

    Finished ex02

--- exercises/required-exercises/sem02/lista02/lista_02.c
+++ exercises/required-exercises/sem02/lista02/lista_02.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <locale.h>
 #define true 1
 #define false 0
 #define bool int
@@ -28,10 +29,10 @@ void ex01() {
 
     printf("Starting exercise exercise 01:\n");
 
-    printf("How many addresses does your computer have.\n");
+    printf("Digite a quantidade de endereços na memória principal.\n");
     scanf("%llu", &addresses);
     
-    printf("Tell me the computer's cell size are.\n");
+    printf("Digite o tamanho das celulas do computador.\n");
     scanf("%llu", &addressBitSize);
     
     memorySize = addresses * addressBitSize;
@@ -40,13 +41,32 @@ void ex01() {
     printf("the memory has a capacity of: ");
     convertToNotation(memorySize);
     printf("\n");
-    printf("The REM size is: %.0LF\n B", log2l(memorySize));
+    printf("The address size is: %.0LF B\n", log2l(memorySize));
+    printf("O tamanho do REM é: %.0LF B\n", log2l(memorySize));
+    printf("O tamanho do RDM é: ");
+    convertToNotation(addressBitSize);
+    printf("\n");
     return;    
 }
 
 void ex02() {
-    printf("Starting exercise exercise 02:\n");
+    unsigned long long lineNumber;
+    unsigned long long lineSize;
+    unsigned long long memorySize;
+    //cell amount = 2 ^ address size
+    printf("Starting exercise 02:\n");
 
+    printf("please tell the principal memory size in bytes:\n");
+    scanf("%lld", &memorySize);
+    printf("Please tell how many lines the cache has:\n");
+    scanf("%lld", &lineNumber);
+    printf("please tell the cache line size:\n");
+    scanf("%lld", &lineSize);
+    
+    printf("tag: %.0Lf\tlinha: %.0Lf\t palavra: %.0Lf\n", log2l(lineNumber/lineSize), log2l(lineNumber), log2l(memorySize));
+    // O tamanho da memória principal em bytes;
+    // O número de linhas da memória cache;
+    // A largura da linha da memória cache em bytes;
     return;    
 }
 
@@ -64,7 +84,8 @@ void ex04() {
 
 
 int main() {
-    ex01();
+    setlocale(LC_ALL, "Portuguese");
+    // ex01();
     ex02();
     ex03();
     ex04();
