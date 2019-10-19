861e9b4f43ac2d91295bf559f94b754c1d9f5729
Rakanamatata <lucasdidek447@gmail.com>
2019-10-18 22:59:07 -0300

    finished exercises

--- exercises/required-exercises/sem02/lista02/lista_02.c
+++ exercises/required-exercises/sem02/lista02/lista_02.c
@@ -7,8 +7,9 @@
 #define false 0
 #define bool int
 
-char* convertToNotation(float number) {
+char* convertToNotation(float number, int type) {
     char* notations[4] = {"B","KB", "MB", "GB"};
+    char* notationsAlt[4] = {"b","Kb", "Mb", "Gb"};
     int numSize = 0;
     bool done = false;
     while(!done) {
@@ -17,7 +18,7 @@ char* convertToNotation(float number) {
             numSize++;
         } else {
             done = true;
-            printf("%.3f %s",number, notations[numSize] );
+            printf("%.3f %s",number, type == 1 ? notations[numSize] : notationsAlt[numSize] );
         }
     }
 }
@@ -27,7 +28,7 @@ void ex01() {
     unsigned long long addressBitSize;
     unsigned long long memorySize;
 
-    printf("Starting exercise exercise 01:\n");
+    printf("Starting exercise 01:\n");
 
     printf("Digite a quantidade de endereços na memória principal.\n");
     scanf("%llu", &addresses);
@@ -39,14 +40,13 @@ void ex01() {
 
     // printf("%lld %s\n",memorySize, convertToNotation(memorySize));
     printf("the memory has a capacity of: ");
-    convertToNotation(memorySize);
+    convertToNotation(memorySize, 1);
     printf("\n");
-    printf("The address size is: %.0LF B\n", log2l(memorySize));
-    printf("O tamanho do REM é: %.0LF B\n", log2l(memorySize));
-    printf("O tamanho do RDM é: ");
-    convertToNotation(addressBitSize);
+    printf("The address size is: %.0LF b\n", log2l(memorySize));
+    printf("O tamanho do REM é: ");
+    convertToNotation(addressBitSize, 0);
     printf("\n");
-    return;    
+    printf("O tamanho do RDM é: %.0LF b\n", log2l(addresses)); 
 }
 
 void ex02() {
@@ -63,32 +63,87 @@ void ex02() {
     printf("please tell the cache line size:\n");
     scanf("%lld", &lineSize);
     
-    printf("tag: %.0Lf\tlinha: %.0Lf\t palavra: %.0Lf\n", log2l(lineNumber/lineSize), log2l(lineNumber), log2l(memorySize));
+    printf("tag: %.0Lf\tlinha: %.0Lf\t palavra: %.0Lf\n",log2l(memorySize) - (log2l(lineNumber) + log2l(lineSize)), log2l(lineNumber), log2l(lineSize));
     // O tamanho da memória principal em bytes;
     // O número de linhas da memória cache;
     // A largura da linha da memória cache em bytes;
-    return;    
 }
 
-void ex03() {
-    printf("Starting exercise exercise 03:\n");
+void ex03(){
+    float mp, cc, mc, td, qlinha, qbloco, tt, t, endbloco;
+    printf("Insira o tamanho da memória principal em bytes: \n");
+    scanf("%f", &mp);
+    printf("Insira a capacidade de memória cache em bytes: \n");
+    scanf("%f", &cc);
+    printf("Insira a largura das linhas da memória cache em bytes: \n");
+    scanf("%f", &mc);
 
-    return;    
+    td = cc * 8;
+    qlinha = cc / mc;
+    qbloco = mp / mc;
+    endbloco = log2l (qbloco);
+    tt = qlinha * endbloco;
+    t = td + tt;
+
+    printf("A memória cache necessita de %.2f Kbits\n", t);
 }
+void ex04(){
 
-void ex04() {
-    printf("Starting exercise exercise 04:\n");
+    float mp, cc, mc, td, qlinha, qbloco, tt, t, x, c, ltag;
+    printf("Insira o tamanho da memória principal em bytes: \n");
+    scanf("%f", &mp);
+    printf("Insira a capacidade de memória cache em bytes: \n");
+    scanf("%f", &cc);
+    printf("Insira a largura das linhas da memória cache em bytes: \n");
+    scanf("%f", &mc);
+    printf("Insira a quantidade de linhas por conjunto: \n");
+    scanf("%f", &x);
 
-    return;    
+    td = cc * 8;
+    qlinha = cc / mc;
+    c = qlinha / x;
+    qbloco = mp / mc;
+    ltag = log2l (qbloco/ c);
+    tt = qlinha * ltag;
+    t = td + tt;
+    printf("A quantidade necessaria para a implementação de cache com Mapeamento associativo por conjunto é de %.2f bits \n", t);
 }
 
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    // ex01();
-    ex02();
-    ex03();
-    ex04();
+    int selector;
+    bool done = false;
+    while(!done) {
+        printf("Qual exercicio vós desejais executar neste atual momento:\n");
+        printf("\t0 - Sair\n\t1 - exercicio 1\n\t2 - exercicio 2\n\t3 - exercicio 3\n\t4 - exercicio 4\n\t");
+        scanf("%d", &selector);
+
+        switch (selector) {
+            case 0:
+                printf("Obrigado por usar nosso programa!\n =)\n");
+                return 1;
+            case 1:
+                ex01();
+                done = true;
+                break;
+            case 2:
+                ex02();
+                done = true;
+                break;
+            case 3:
+                ex03();
+                done = true;
+                break;
+            case 4:
+                ex04();
+                done = true;
+                break;
+            default:
+                printf("Esse exercicio não existe.\n");
+                break;
+        }
+    }
+    
     return 0;
 }
-
